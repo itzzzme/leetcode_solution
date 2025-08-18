ef032f52e1851aa6560cf5cb74b829da2b6c4a98
Sayan <136343762+itzzzme@users.noreply.github.com>
2025-08-18 13:29:57 +0530

    Update Climbing-Stairs.cpp

--- dp/Climbing-Stairs.cpp
+++ dp/Climbing-Stairs.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n + 1, 0);
         if(n<=2) return n;
+        vector<int> dp(n + 1, 0);
         dp[0] = 0, dp[1] = 1, dp[2] = 2;
-        if (n <= 2)
-            return n;
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
