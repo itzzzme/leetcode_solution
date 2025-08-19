e7707ba17ef5c99d427eb3b66a605e94f737473f
Sayan <136343762+itzzzme@users.noreply.github.com>
2025-08-19 12:19:36 +0530

    [646] [Time Beats: 50.78%] [Memory Beats: 54.77%] - LeetPush

--- dp/Maximum-Length-of-Pair-Chain.cpp
+++ dp/Maximum-Length-of-Pair-Chain.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(vector<vector<int>>& v, int prev, int i) {
-        if (i >= v.size())
-            return 0;
-        int take = 0;
-        if (dp[i][prev + 1] != -1)
-            return dp[i][prev + 1];
-        if (prev == -1 or v[i][0] > v[prev][1])
-            take = 1 + solve(v, i, i + 1);
-        int skip = solve(v, prev, i + 1);
-        return dp[i][prev + 1] = max(take, skip);
-    }
+    // vector<vector<int>> dp;
+    // memoization + recursion
+    // int solve(vector<vector<int>>& v, int prev, int i) {
+    //     if (i >= v.size())
+    //         return 0;
+    //     int take = 0;
+    //     if (dp[i][prev + 1] != -1)
+    //         return dp[i][prev + 1];
+    //     if (prev == -1 or v[i][0] > v[prev][1])
+    //         take = 1 + solve(v, i, i + 1);
+    //     int skip = solve(v, prev, i + 1);
+    //     return dp[i][prev + 1] = max(take, skip);
+    // }
     int findLongestChain(vector<vector<int>>& pairs) {
         int n = pairs.size();
-        dp.assign(n, vector<int>(n + 1, -1));
+        // dp.assign(n, vector<int>(n + 1, -1));
         sort(pairs.begin(), pairs.end());
-        return solve(pairs, -1, 0);
+        vector<int> dp(n, 1);
+        // bottom-up approach
+        int mx = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (pairs[j][1] < pairs[i][0]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                    mx = max(dp[i], mx);
+                }
+            }
+        }
+        // return solve(pairs, -1, 0);
+        return mx;
     }
 };
