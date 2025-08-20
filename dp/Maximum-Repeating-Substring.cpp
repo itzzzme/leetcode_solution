29c97a3a0fff5f4f8e25689fab5015fd2850cf1a
Sayan <136343762+itzzzme@users.noreply.github.com>
2025-08-20 21:20:56 +0530

    [1668] [Time Beats: 100.00%] [Memory Beats: 83.23%] - LeetPush

--- dp/Maximum-Repeating-Substring.cpp
+++ dp/Maximum-Repeating-Substring.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     int maxRepeating(string sequence, string word) {
-        int n = sequence.size();
-        int m = word.size();
-        vector<int> dp(n, 0);
-        int ans = 0;
-        for (int i = m - 1; i < n; i++) {
-            if (sequence.substr(i - m + 1, m) == word) {
-                if (i - m >= 0)
-                    dp[i] = dp[i - m] + 1;
-                else
-                    dp[i] = 1;
-                ans = max(ans, dp[i]);
-            }
+        // int n = sequence.size();
+        // int m = word.size();
+        // vector<int> dp(n, 0);
+        // int ans = 0;
+        // for (int i = m - 1; i < n; i++) {
+        //     if (sequence.substr(i - m + 1, m) == word) {
+        //         if (i - m >= 0)
+        //             dp[i] = dp[i - m] + 1;
+        //         else
+        //             dp[i] = 1;
+        //         ans = max(ans, dp[i]);
+        //     }
+        // }
+        // return ans;
+
+        // simple approach without dp
+        string temp = word;
+        int cnt = 0;
+        while (sequence.find(temp) != string::npos) {
+            cnt++;
+            temp += word; 
         }
-        return ans;
+        return cnt;
     }
 };
