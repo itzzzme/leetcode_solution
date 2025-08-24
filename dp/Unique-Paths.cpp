e2081de501fc80e0fc17a952f05b0273207888b5
Sayan <136343762+itzzzme@users.noreply.github.com>
2025-08-24 19:57:24 +0530

    [62] [Time Beats: 18.47%] [Memory Beats: 8.82%] - LeetPush

---
+++ dp/Unique-Paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // memoization + recursion
+    vector<vector<int>> dp;
+    int solve(int i, int j, int m, int n) {
+        if (i >= m or j >= n)
+            return 0;
+        if (i == m - 1 and j == n - 1)
+            return 1;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        return dp[i][j] = solve(i + 1, j, m, n) + solve(i, j + 1, m, n);
+    }
+    int uniquePaths(int m, int n) {
+        dp.resize(m+1,vector<int>(n+1,-1));
+        return solve(0,0,m,n);
+    }
+};
