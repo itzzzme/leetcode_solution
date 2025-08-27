f5ac92434b524499d2b60821c27706fa5a63e339
Sayan <136343762+itzzzme@users.noreply.github.com>
2025-08-27 11:11:08 +0530

    [357] [Time Beats: 100.00%] [Memory Beats: 16.77%] - LeetPush

--- dp/Count-Numbers-with-Unique-Digits.cpp
+++ dp/Count-Numbers-with-Unique-Digits.cpp
@@ -1,13 +1,38 @@
 class Solution {
 public:
+    // solving without dp
+
+    // int countNumbersWithUniqueDigits(int n) {
+    //     if (n == 0)
+    //         return 1;
+    //     int res = 10, cur = 9;
+    //     for (int i = 2; i <= n && i <= 10; ++i) {
+    //         cur *= (11 - i);
+    //         res += cur;
+    //     }
+    //     return res;
+    // }
+
+    // solving with dp
     int countNumbersWithUniqueDigits(int n) {
         if (n == 0)
             return 1;
-        int res = 10, cur = 9;
-        for (int i = 2; i <= n && i <= 10; ++i) {
-            cur *= (11 - i);
-            res += cur;
+        n = min(n, 10);
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 10;
+        for (int i = 2; i <= n; ++i) {
+            int choices = 9;
+            int available = 9;
+            for (int j = 1; j < i; ++j) {
+                choices *= available;
+                available--;
+            }
+            dp[i] = choices;
         }
-        return res;
+        int result = 0;
+        for (int i = 0; i <= n; ++i)
+            result += dp[i];
+        return --result;
     }
 };
