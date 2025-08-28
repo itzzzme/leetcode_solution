27e2f37c684ade2ea31a257e52baaef2982c77da
Sayan <136343762+itzzzme@users.noreply.github.com>
2025-08-28 21:36:06 +0530

    [709] [Time Beats: 100.00%] [Memory Beats: 81.09%] - LeetPush

---
+++ strings/To-Lower-Case.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for (auto& i : s) 
+            if (i >= 'A' and i<='Z')
+                i += 32;
+        return s;
+    }
+};
