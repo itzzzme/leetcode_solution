e0ef9edad831716264b09d9337725a1b464646ee
Sayan <136343762+itzzzme@users.noreply.github.com>
2025-09-04 12:19:28 +0530

    Update Maximum-Subarray.cpp

--- dp/Maximum-Subarray.cpp
+++ dp/Maximum-Subarray.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        // kadane's algo
         int mx = nums[0], curr = nums[0];
         for (int i = 1; i < nums.size(); i++) {
             curr = max(nums[i], nums[i] + curr);
             mx = max(curr, mx);
         }
         return mx;
+
+        // also using dp
+        // vector<int> dp(nums.size(),0);
+        // int mx=nums[0];
+        // dp[0]=nums[0];
+        // for(int i=1;i<nums.size();i++){
+        //     dp[i]=max(nums[i],nums[i]+dp[i-1]);
+        //     mx=max(dp[i],mx);
+        // }
+        // return mx;
     }
 };
